65cedeac5851574c6aeb3f3cb8ddf1fa94ace51c
Shaked178 <aesgbcr@gmail.com>
2025-11-18 22:35:14 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,27 @@
-def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+def split_before_each_uppercase(formula):
+    if not formula:
+        return []
+    
+    result = []
+    current_word = ""
+
+    for char in formula:
+        if 'A' <= char <= 'Z':
+            if current_word:
+                result.append(current_word)
+            current_word = char
+        else:
+            current_word += char
+            
+    if current_word:
+        result.append(current_word)
+        
+    return result
 
 
-def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+def split_at_digit(s):
+    for i, ch in enumerate(s):
+        if ch.isdigit():
+            return s[:i], int(s[i:])
+    return s, 1
+
